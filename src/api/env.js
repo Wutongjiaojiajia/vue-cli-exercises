18efc72aa115929e586f8f146f85ee701b969577
黄家豪 <490899577@qq.com>
2020-03-24 09:35:10 +0800

    更新http方法

---
+++ src/api/env.js
@@ -0,0 +1,11 @@
+import appenv from './appenv';
+const env={
+    qas:{
+        test:'https://easy-mock.com/mock/5c4c50b9888ef15de01bec2c/api'
+    },
+    app:{
+        test:'https://easy-mock.com/mock/5c4c50b9888ef15de01bec2c/api'
+    }
+};
+
+export default env[appenv];
---
+++ src/api/index.js
@@ -0,0 +1,52 @@
+import http from './http';
+import env from './env';
+import { MessageBox } from "element-ui";
+
+/**
+ * url: 地址
+ * method: 请求方法
+ * params: 参数
+ * timeout: 超时时间
+ * isOriginalGET: 是否传统get传参
+ */
+
+
+// 退出登录回调
+const logoutCallback = (info) => {
+    MessageBox.alert(info, {
+        showClose: false,
+        confirmButtonText: "确定",
+        customClass: "alertFailure"
+    });
+} 
+
+// 更换 token 回调
+const changeJwtCallback = (jwt) => {
+    
+}
+
+// 接口无权限回调
+const noAuthCallback = (info) => {
+    MessageBox.alert(info, {
+        showClose: false,
+        confirmButtonText: "确定",
+        customClass: "alertFailure",
+    });
+}
+
+const req = ({ baseUrl, method, url, params, timeout, isOriginalGET}) => {
+    let options={
+        url: env[baseUrl] + url,
+        method: method, 
+        params: params, 
+        timeout: timeout, 
+        isOriginalGET: isOriginalGET, 
+        logoutCallback: logoutCallback,
+        changeJwtCallback: changeJwtCallback,
+        noAuthCallback: noAuthCallback
+    };
+
+    return http(options);
+}
+
+export default req;
